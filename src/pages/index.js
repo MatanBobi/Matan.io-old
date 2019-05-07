8618667ff86ddb8f029a626eeaf7a06e2af79358
Thomas Wang <iothomaswang@gmail.com>
2019-05-07 03:30:05 +0100

    change copy and readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,9 +24,8 @@ class IndexPage extends React.Component {
         </h1>
         <p>Welcome to your new Gatsby website. You are on your home page.</p>
         <p>
-          This starter comes out of the box with styled components, dark mode,
-          and Gatsby's default starter blog running on Netlify CMS (managed at{" "}
-          <Link to="/admin/">/admin</Link>).
+          This starter comes out of the box with styled components and Gatsby's
+          default starter blog running on Netlify CMS.
         </p>
         <p>Now go build something great!</p>
         <Link to="/blog/">
